56da4c11eda4ede24fad64aba4672bb835ea01f6
Mart van de Ven <m@type.hk>
2015-06-08 17:06:40 +0800

    Exit Tickets

--- admin/exitticket.py
+++ admin/exitticket.py
@@ -3,9 +3,10 @@ import pandas as pd
 import sys 
 import urllib
 
-BASE_URL = "https://docs.google.com/forms/d/1FOGs32T-0DZSwOHE-1r8rP6m4fI3fYxSC9jmd7rd7ak/viewform?entry.902636390="
+
+BASE_URL = "https://docs.google.com/forms/d/1KTCWUQJKL5WmpkT_ZlG-eqcrtn3QSgdWhRzaT57PFsY/viewform?entry.902636390="
 INTERFIX = "&entry.1478388549="
-SUFFIX = "&entry.1994883592&entry.1985262344&entry.1394458725&entry.563048722"
+SUFFIX = "&entry.1994883592&entry.1394458725&entry.563048722"
 emails = pd.read_csv('emails.csv', names=['address'])
 classes = pd.read_csv('classes.csv', names=['topic'])
 topic = classes.topic.values[int(sys.argv[1]) - 1] 
